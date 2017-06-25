c6ed454686128f2c2f1f38ec983ac01f31090e53
Keir Nellyer <keir@nellyer.co.uk>
2017-06-25 17:28:43 +0100

    Some more restructuring in effort to get a near perfect structure/flow

--- src/main/java/com/keirnellyer/portfolio/WebApplication.kt
+++ src/main/java/com/keirnellyer/portfolio/WebApplication.kt
@@ -1,52 +1,35 @@
 package com.keirnellyer.portfolio
 
-import com.keirnellyer.portfolio.model.Profile
+import com.keirnellyer.portfolio.controller.PortfolioController
 import com.keirnellyer.portfolio.repository.DummyProfileRepository
 import com.keirnellyer.portfolio.repository.ProfileRepository
-import com.keirnellyer.portfolio.view.ProfileView
-import com.keirnellyer.portfolio.template.SparkHelper
 import com.keirnellyer.portfolio.view.AppConfig
-import spark.ModelAndView
-import spark.Request
+import freemarker.template.Configuration
+import no.api.freemarker.java8.Java8ObjectWrapper
 import spark.TemplateEngine
-import spark.kotlin.get
 import spark.kotlin.staticFiles
+import spark.template.freemarker.FreeMarkerEngine
 
-class WebApplication(val templateEngine: TemplateEngine) {
-    var appConfig: AppConfig = AppConfig(1337)
+class WebApplication {
+    companion object {
+        val appConfig: AppConfig = AppConfig(1337)
+        val templateEngine: TemplateEngine
+
+        init {
+            // configure and set template engine
+            val configuration: Configuration = Configuration(Configuration.VERSION_2_3_26)
+            configuration.setClassForTemplateLoading({}::class.java, "/templates")
+            configuration.objectWrapper = Java8ObjectWrapper(Configuration.VERSION_2_3_26)
+            templateEngine = FreeMarkerEngine(configuration)
+        }
+    }
+
     val profileRepository: ProfileRepository = DummyProfileRepository()
 
     init {
         staticFiles.location("/public")
 
-        get("/") {
-            val model: MutableMap<String, Any> = HashMap()
-            model.put("profile", view(profileRepository.profile))
-            render(request, model, "index.ftl")
-        }
+        PortfolioController(appConfig, profileRepository).initializeRoutes()
     }
 
-    fun view(model: Profile): ProfileView {
-        // obfuscate email so it isn't shown in plaintext (client-side JS will handle de-obfuscating)
-        val obfuscatedEmail = xorString(model.email)
-
-        return ProfileView(model.firstName, model.lastName, model.age, model.jobTitle, model.location,
-                obfuscatedEmail)
-    }
-
-    fun xorString(s: String): String {
-        // encode the string by XOR'ing each char
-        // this can be decoded by repeating the operation
-        return s.toCharArray()
-                .map { c: Char -> c.toInt() xor appConfig.obfuscationKey }
-                .map { i: Int -> i.toChar() }
-                .joinToString(separator = "")
-    }
-
-    fun render(request: Request, model: Map<String, Any>, templatePath: String): String {
-        val mutableModel: MutableMap<String, Any> = HashMap(model)
-        mutableModel.put("appConfig", appConfig)
-        mutableModel.put("fw", SparkHelper(request))
-        return templateEngine.render(ModelAndView(mutableModel, templatePath))
-    }
 }
---
+++ src/main/java/com/keirnellyer/portfolio/controller/PortfolioController.kt
@@ -0,0 +1,36 @@
+package com.keirnellyer.portfolio.controller
+
+import com.keirnellyer.portfolio.repository.ProfileRepository
+import com.keirnellyer.portfolio.view.AppConfig
+import com.keirnellyer.portfolio.view.ProfileView
+import com.keirnellyer.portfolio.view.ViewUtil
+import spark.kotlin.get
+import spark.kotlin.RouteHandler
+
+class PortfolioController(val appConfig: AppConfig, val profileRepository: ProfileRepository) {
+    fun initializeRoutes() {
+        get("/", function = servePortfolioPage)
+    }
+
+    val servePortfolioPage: RouteHandler.() -> Any = {
+        val model: MutableMap<String, Any> = HashMap()
+
+        val profile = profileRepository.profile
+        // obfuscate email so it isn't shown in plaintext (client-side JS will handle de-obfuscating)
+        val obfuscatedEmail = xorString(profile.email)
+        val profileView = ProfileView(profile.firstName, profile.lastName, profile.age, profile.jobTitle,
+                profile.location, obfuscatedEmail)
+
+        model.put("profile", profileView)
+        ViewUtil.render(request, model, "index.ftl")
+    }
+
+    private fun xorString(s: String): String {
+        // encode the string by XOR'ing each char
+        // this can be decoded by repeating the operation
+        return s.toCharArray()
+                .map { c: Char -> c.toInt() xor appConfig.obfuscationKey }
+                .map { i: Int -> i.toChar() }
+                .joinToString(separator = "")
+    }
+}
