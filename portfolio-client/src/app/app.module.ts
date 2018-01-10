45c273306a02524b134466a44d3599331cf8c132
Keir Nellyer <keir@nellyer.co.uk>
2018-01-10 20:13:54 +0000

    Setup basic Angular structure, got the site rendering static content

--- portfolio-client/src/app/app.module.ts
+++ portfolio-client/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { SiteFooterComponent } from './site-footer/site-footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    SiteFooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- portfolio-server/src/main/java/com/keirnellyer/portfolio/controller/PublicController.java
+++ portfolio-server/src/main/java/com/keirnellyer/portfolio/controller/PublicController.java
@@ -1,14 +1,8 @@
 package com.keirnellyer.portfolio.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PublicController {
-    @RequestMapping
-    public String index(Model model) {
-        return "templates/index";
-    }
+
 }
