3b9aa5c44b69b1847b50883415a0795f1a11dfe4
Keir Nellyer <keir@nellyer.co.uk>
2018-01-16 10:51:30 +0000

    Wire up some parts of the header to dynamic data from the backend

--- portfolio-client/src/app/site-header/site-header.component.ts
+++ portfolio-client/src/app/site-header/site-header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Site } from '../site';
 
 @Component({
   selector: 'site-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./site-header.component.css']
 })
 export class SiteHeaderComponent implements OnInit {
+
+  @Input() site: Site;
 
   constructor() { }
 
